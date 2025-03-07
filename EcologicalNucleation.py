7a3a2029722b04e71829bab081f80b99f46b568f
sdebesai <serenadebesai@gmail.com>
2025-03-06 18:42:48 -0600

    with FFS class, untested

--- EcologicalNucleation.py
+++ EcologicalNucleation.py
@@ -14,26 +14,28 @@ import pickle as pkl
 #A: SxS matrix
 
 class EcoSim():
-    def __init__(self, N, S, m, T_tot, mu, A, rates, num_migrants):
+    def __init__(self, N, S, m, T_tot, mu, A, rates, num_migrants, curr_lambda):
         self.N=N 
         self.S=S 
-        self.T_tot=T_tot
-        self.delta_T=np.random.exponential(scale=m, size=T_tot)#generate mutation events
-        rng = np.random.default_rng()
+        self.T_tot=T_tot #this is in units of migration events
+        self.m=m
+        self.mu=mu
+        #self.delta_T=np.random.exponential(scale=m, size=T_tot)#generate mutation events
+        #rng = np.random.default_rng()
         #pick the order of arrivals
-        self.migrants=rng.choice(S, size=T_tot, replace=True, p=mu)
+        #self.migrants=rng.choice(S, size=T_tot, replace=True, p=mu)
         #A_ii=1 and A_ij is a randomly sampled number
         self.A=A
         self.rates=rates
-        self.num_migrants=1
         self.N_v_t=np.zeros((self.T_tot, self.S))
         self.T=np.zeros(self.T_tot+1)
         self.all_times=np.array([])
         self.all_solns=np.array([self.N])
         self.num_migrants=num_migrants
+        self.curr_lambda=curr_lambda
 
     def LV_dynamics(self, t, x):
-        return np.multiply(x, self.rates + np.matmul(self.A, x))
+        return np.multiply(x, self.rates + np.matmul(self.A, x)) #dx_i/dt = x_i(r + Ax)_i
     
     def grow(self, ti, tf):
         sol=scipy.integrate.solve_ivp(self.LV_dynamics, t_span=(ti, tf), y0=self.N, dense_output=True)
@@ -41,6 +43,8 @@ class EcoSim():
     
     def full_simulate(self):
         #simulate until you hit the next population size
+
+        '''
         for i, t in enumerate(self.T[:-1]):
             self.N_v_t[i]=self.N #2D array where A_ij=the count of species j at time i
             self.T[i+1]=self.T[i]+self.delta_T[i]
@@ -50,30 +54,72 @@ class EcoSim():
             times, solns, self.N=self.grow(self.T[i], self.T[i+1])
             self.all_times=np.append(self.all_times, times)
             self.all_solns=np.vstack((self.all_solns, solns.T))
-        
-        return self.N_v_t, self.T, self.all_times, self.all_solns
+        '''
+        i=0
+        while (np.sum(self.N) < self.curr_lambda and np.sum(self.N) > 0) and i < self.T_tot-1:
+            self.N_v_t[i]=self.N #2D array where A_ij=the count of species j at time i
+            delta_T=np.random.exponential(scale=self.m, size=1)
+            
+            rng = np.random.default_rng() #pick the next migrant
+            migrant=rng.choice(self.S, size=1, p=self.mu)
+            
+            self.N[migrant]=self.N[migrant]+self.num_migrants #increment migrant pop
 
-#FFS sampling
-'''
+            self.T[i+1]=self.T[i]+delta_T #update time
+
+            #growth subject to LV dynamics
+            times, solns, self.N=self.grow(self.T[i], self.T[i+1])
+            self.all_times=np.append(self.all_times, times)
+            self.all_solns=np.vstack((self.all_solns, solns.T))
+            i=i+1
+
+        if np.sum(self.N) < self.curr_lambda and np.sum(self.N) > 0:
+            success=True
+
+        return self.N_v_t, self.T, self.all_times, self.all_solns, i, success
+
+#FFS wrapper
+
 class FFS():
-    def __init__(self, lambdas, S, num_trajs, curr_lambda, A, m, mu, rates):
-        self.lambdas=lambdas #lambdas
+    def __init__(self, lambdas, S, num_trajs, A, m, mu, rates):
+        self.lambdas=lambdas #array of lambdas
         self.S=S #number of species
         self.num_trajs=num_trajs #number of trajectories to launch from each interface
-        self.curr_lambda=curr_lambda
-        self.T_tot=0
-        self.N=0
-        self.A=A
-        self.m=m
-        self.mu=mu
-        self.rates=rates
-
-    def next_lambda(self):
-        es=EcoSim(self.N, self.S, self.m, self.T_tot, self.mu, self.A, self.rates) #instantiate a simulation
+        
+        self.T_tot=0 #max time for each simulation
+        self.A=A #interaction matrix
+        self.m=m #relative migration rates
+        self.mu=mu #total migration rate
+        self.rates=rates #individual growth rates
+        self.N_curr_lambda= np.empty((self.num_trajs, self.S+4))#2D array: num_trajs x num_species + 4
+        self.N_prev_lambda= np.empty((self.num_trajs, self.S+4))#2D array: num_trajs x num_species + 4
+        
+        self.N_prev_lambda[:]=0 #initialize previous to 0, except success column is 1
+        self.N_prev_lambda[:, -1]=1
     
-    def store_traj_configs():
-'''
+    def next_lambda(self, curr_lambda):
+        init_idx_choices=np.where(self.N_prev_lambda[:, -1] == 1) #get the possible initial indexes
 
+        rng = np.random.default_rng()
+        init_idx_choices=rng.choice(init_idx_choices, size=self.num_trajs, replace=True) #sample uniformly from these possible iniitial states
+
+        #iterate through the trajs
+        for t, idx in enumerate(init_idx_choices):
+            N=self.N_prev_lambda[t][0:self.S-1] #get the initial configuration
+            es=EcoSim(N, self.S, self.m, self.T_tot, self.mu, self.A, self.rates, curr_lambda) #instantiate a simulation
+            N_v_t, T, all_times, all_solns, num_mig_events, success=es.full_simulate() #run the simulation
+            self.N_prev_lambda[t]=np.append(N_v_t[-1], np.array([N_v_t, np.sum(N_v_t), idx, num_mig_events, success])) #update previous lambda array
+
+    #dump the prev lambda data
+    def save_state(self, curr_lambda):
+        with open(f'{curr_lambda}_data.pkl', 'wb') as f:
+            pkl.dump(self.N_prev_lambda, f)
+            f.close()
+
+    def run_FFS(self):
+        for curr_lambda in self.lambdas: #iterate through every lambda
+            self.save_state(curr_lambda) #save the previous lambda
+            self.next_lambda(curr_lambda) #sample for next lambda
 
 '''
 def init_sim(S, T_tot, m, mu, max_r):
